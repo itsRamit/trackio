b46ee9c3bd629fac158829ff7bddb5e03b58b703
itsRamit <ece22145@iiitkalyani.ac.in>
2025-01-26 15:41:17 +0530

    added email notifier pannel & UI fixes of home page

--- src/script.js
+++ src/script.js
@@ -2,3 +2,17 @@ function scrollToSection() {
     const target = document.getElementById('target-section');
     target.scrollIntoView({ behavior: 'smooth' });
 }
+
+document.getElementById('track-button').addEventListener('click', function () {
+    const emailForm = document.getElementById('email-form');
+    // Toggle visibility of the email form
+    emailForm.classList.toggle('hidden');
+});
+
+const closeButton = document.getElementById('close-email-form');
+const emailForm = document.getElementById('email-form');
+
+closeButton.addEventListener('click', () => {
+    // Use the class toggle method for consistent hiding
+    emailForm.classList.add('hidden');
+});
