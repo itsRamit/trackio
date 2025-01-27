45990eda4fdf6779658b86978d7cbd00136a5ce6
itsRamit <ece22145@iiitkalyani.ac.in>
2025-01-27 12:59:31 +0530

    fix: adjust button padding and color for better UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         'custom-gradient' : 'linear-gradient(to right, #5F0F40, #310E68)',
-      }
+      },
+      colors: {
+        customTeal: '#00C8B5',
+      },
       
     },
   },
