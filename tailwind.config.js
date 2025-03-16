efc672839f1f34777de59635e8e9f332101bf694
itsRamit <ece22145@iiitkalyani.ac.in>
2025-03-16 17:31:34 +0530

    fix: Navbar hover effect and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'custom-gradient' : 'linear-gradient(to right, #5F0F40, #310E68)',
+        'neon-gradient': 'linear-gradient(to right, #00FF88, #00E5FF)'
       },
       colors: {
         customTeal: '#00C8B5',
